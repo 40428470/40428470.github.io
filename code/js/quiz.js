2f7585866579c463d9f4bb07f43a3eacbe86dfaa
Callum Turino <40428470@live.napier.ac.uk>
2022-04-24 19:49:55 +0100

    java changes

--- code/js/quiz.js
+++ code/js/quiz.js
@@ -1,34 +1,61 @@
 // JavaScript source code
 // JavaScript source code
 /*event listener code based on https://sebhastian.com/call-javascript-function-html/#:~:text=To%20call%20a%20JavaScript%20function,getElementById()%20method.8 */
-const correctArray = ["quest-1-d", "quest-2-b", "quest-3-c", "quest-4-a", "quest-5-b", "quest-6-b", "quest-7-d", "quest-8-c", "quest-9-c", "quest-10-d", "quest-11-a", "quest-12-b", "quest-13-a", "quest-14-1", "quest-15-c"];
-ansArray = []
-function checkAnswer_1 (correctArray, AnsArray) {
+const correctAns = [
+    'quest-1-d',
+    "quest-2-b",
+    "quest-3-c",
+    "quest-4-a",
+    "quest-5-b",
+    "quest-6-b",
+    "quest-7-d",
+    "quest-8-c",
+    "quest-9-c",
+    "quest-10-d",
+    "quest-11-a",
+    "quest-12-b",
+    "quest-13-a",
+    "quest-14-1",
+    "quest-15-c"
+];
+const userAns = [];
+
+function checkAnswer_1() {
     //checking question answer
     var i = 0
-    if (document.getElementById(correctArray[i]).checked) {
+
+    if (document.getElementById(correctAns[0]).checked) {
         var quest1 = "correct";
-        ansArray[0] = quest1;
+        userAns[0] = quest1;
         console.log("correct")
-        console.log(ansArray)
-    } else if (document.getElementById("quest-1-a").checked || document.getElementById("quest-1-b").checked || document.getElementById("quest-1-c").checked){
-        var quest1 = "wrong";
-        ansArray[0] = quest1;
-        console.log("wrong")
-        console.log(ansArray)
+        console.log(userAns);
     }
-    return ansArray;
+    else if (document.getElementById("quest-1-a").checked || document.getElementById("quest-1-b").checked || document.getElementById("quest-1-c").checked) {
+        var quest1 = "wrong";
+        userAns[0] = quest1;
+        console.log("wrong");
+        console.log(userAns);
+    }
+    //return userAns;
 
 
 }
 
 
 
-function storeAnswer_1(ansArray) {
-    i = 0
-    for (i; i + 1; 4) {
-        y = String(i);
-        sessionStorage.setItem(y, ansArray[i]);
-    }
-
+function storeAnswer_1(userAns) {
+    var y = 1;
+    var i = 0;
+    console.log(userAns[0])
+    //while (i < 3) {
+        var y_string = y.toString();
+        console.log(i)
+        console.log(userAns[i]);
+    sessionStorage.setItem(y_string, userAns[i]);
+    sessionStorage.setItem("test", "works");
+        var y = y + 1;
+        var i = i + 1;
+    //}
+    console.log(sessionStorage.length)
+    console.log(sessionStorage.getItem("test"));
 }
