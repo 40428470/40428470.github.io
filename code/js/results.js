a56fb2d321292d67894e43f6eaa2e5d2a8492a57
Turino <40428470@live.napier.ac.uk>
2022-04-26 13:34:13 +0100

    results js cjamhes

--- code/js/results.js
+++ code/js/results.js
@@ -77,4 +77,97 @@ function results_check() {
             p_3_ele.appendChild(p_3);
             break;
     }
+
+    //check question 4 
+    switch (sessionStorage.getItem("4")) {
+        case "correct":
+            var p_4 = document.createElement("p");
+            var p_4_txt = document.createTextNode("Correct! - Martin Heinrich Klaproth");
+            p_4.appendChild(p_3_txt);
+            var p_4_ele = document.getElementById("quest-4-correct");
+            p_4_ele.appendChild(p_4);
+            break;
+        case "wrong":
+            var p_4 = document.createElement("p");
+            var p_4_txt = document.createTextNode("Incorrect - Try Again");
+            p_4.appendChild(p_4_txt);
+            var p_4_ele = document.getElementById("quest-4-incorrect");
+            p_4_ele.appendChild(p_4);
+            break;
+        case "no-answer":
+            var p4 = document.createElement("p");
+            var p_4_txt = document.createTextNode("You Didn't Answer This One");
+            p_4.appendChild(p_4_txt);
+            var p_4_ele = document.getElementById("quest-4-incorrect");
+            p_4_ele.appendChild(p_4);
+            break;
+    }
+
+    //check question 5
+    switch (sessionStorage.getItem("5")) {
+        case "correct":
+            var p_5 = document.createElement("p");
+            var p_5_txt = document.createTextNode("Correct! - 1986");
+            p_5.appendChild(p_5_txt);
+            var p_5_ele = document.getElementById("quest-5-correct");
+            p_5_ele.appendChild(p_5);
+            break;
+        case "wrong":
+            var p_5 = document.createElement("p");
+            var p_5_txt = document.createTextNode("Incorrect - Try Again");
+            p_5.appendChild(p_5_txt);
+            var p_5_ele = document.getElementById("quest-5-incorrect");
+            p_5_ele.appendChild(p_5);
+            break;
+        case "no-answer":
+            var p5 = document.createElement("p");
+            var p_5_txt = document.createTextNode("You Didn't Answer This One");
+            p_5.appendChild(p_5_txt);
+            var p_5_ele = document.getElementById("quest-5-incorrect");
+            p_5_ele.appendChild(p_5);
+            break;
+    }
+
+    //check question 6
+    switch (sessionStorage("6")) {
+        case "correct":
+            var p_6 = document.createElement("p");
+            var p_6_txt = document.createTextNode("Correct! - 2006");
+            p_6.appendChild(p_6_txt);
+            var p_6_ele = document.getElementById("quest-6-correct");
+            p_6_ele.appendChild(p_6);
+            break;
+        case "wrong":
+            var p_6 = document.createElement("p");
+            var p_6_txt = document.createTextNode("Incorrect - Try Again");
+            p_6.appendChild(p_6_txt);
+            var p_6_ele = document.getElementById("quest-6-incorrect");
+            p_6_ele.appendChild(p_6);
+            break;
+        case "no-answer":
+            var p6 = document.createElement("p");
+            var p_6_txt = document.createTextNode("You Didn't Answer This One");
+            p_6.appendChild(p_6_txt);
+            var p_6_ele = document.getElementById("quest-6-incorrect");
+            p_6_ele.appendChild(p_6);
+            break;
+    }
 }
+
+
+//function test_check() {
+ //   let x = 1;
+ //   while (x <= 14) {
+  //      let x_string = x.toString();
+  //      let div_id_correct = x_string + "-correct";
+//
+   //     var ele_tag = document.createElement("p");
+   //     var ele_txt = document.createTextNode("Correct!");
+    //    ele_tag.appendChild(p_3_txt);
+    //    var ele = document.getElementById(div_id_correct);
+     //   ele.appendChild(ele_tag);
+     //   break;
+
+
+  //  }
+//}
